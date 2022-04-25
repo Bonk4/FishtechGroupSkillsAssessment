6bf7e4a1dced38e3ffbacf1698441273a3d1d802
Dominic Brazeel <caboose271@gmail.com>
2022-04-24 23:19:17 -0500

    got api returning real data

---
+++ App.WebApi/src/controller/whoIsController.ts
@@ -0,0 +1,19 @@
+import { WhoIsResponse } from "../model/whoIsResponse";
+import fetch from "node-fetch";
+
+export class WhoIsController {
+    endpoint = "https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=at_olp4gHQPIeIBuYQkrg2WzPBOcfK4G&outputFormat=json&domainName=";
+
+    //actual WhoIs request using 
+    async get(domain: string): Promise<any> {
+        console.log(this.endpoint + domain);
+        let response: any;
+
+        await (await fetch(this.endpoint + domain, {
+            method: 'GET',
+            headers: { 'Context-Type': 'application/json' }
+        })).json().then(x => response = x);
+        
+        return response;
+    }
+}
